d10860bac953414968f41bac5783f1d150051d5b
OleksandVakhno <vakhno.olexandrlutsk@gmail.com>
2019-04-02 21:53:46 -0500

    modified and uninstaled unneeded packages

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-var fs = require('file-system');
+const fs = require('fs');
+const http = require('http')
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-fs.readfile('index.html', (err,html) => {
+fs.readFile('index.html', (err,html) => {
     if(err){
         throw err;
     }
 
-    const server = createServer((req,res) => {
+    const server = http.createServer((req,res) => {
         res.statusCode = 200;
-        res.setHeader('Content-type', 'text/plain');
+        res.setHeader('Content-type', 'text/html');
         res.write(html);
         res.end();
     })
