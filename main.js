3f46f587cce558a947948a7fb18623aec8ecabe8
OleksandVakhno <vakhno.olexandrlutsk@gmail.com>
2019-04-03 22:05:41 -0500

    Installed express and body-parser, modified main.js

--- main.js
+++ main.js
@@ -3,19 +3,21 @@ const http = require('http')
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-fs.readFile('index.html', (err,html) => {
-    if(err){
-        throw err;
-    }
 
-    const server = http.createServer((req,res) => {
+
+http.createServer((req,res) => {
+    
+    index = fs.readFile('index.html', (err,html) => {
+        if(err){
+            throw err;
+        }
         res.statusCode = 200;
         res.setHeader('Content-type', 'text/html');
         res.write(html);
         res.end();
     })
 
-    server.listen(port, hostname, ()=>{
-        console.log('Server started on port' + port)
-    })
+}).listen(port, hostname, ()=>{
+    console.log('Server started on port' + port)
 })
+
